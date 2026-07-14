43ab2a12d8c07d1ed58ff324da7b646c40a66d63
gkishalan <princekishalan655@gmail.com>
2026-07-14 12:58:06 +0530

    Day 1 Final Project

---
+++ Day1/Final.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.\n")
+
+city =input("What is the name of the city you grew up?\n")
+pet = input("What is your pet's name?\n")
+
+print("Your band name is " + city + " " + pet)
---
+++ Day1/task2.py
@@ -0,0 +1,2 @@
+#The Python Input Function 
+print("Hello " + input("What is your name?"))
