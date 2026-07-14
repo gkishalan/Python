ea705557491a187d48a932835395d1a41c08d7cb
gkishalan <princekishalan655@gmail.com>
2026-07-14 12:47:13 +0530

    String Manipulation

--- Day1/task1.py
+++ Day1/task1.py
@@ -1,9 +1,12 @@
 #Printing the Console in Python 
-print("Hello World!")
+print("Hello World!\n\n\n")
 
 #Printing Practice 
 print("Mix 500g of floor , 10g yeast and 300ml water in a bowel")
 print("Knead the dough for 10 minutes")
 print("Add 3g of salt")
 print("Leave to rise for 2 hours")
-print("Bake at 200 degrees C for 30 Minutes")
+print("Bake at 200 degrees C for 30 Minutes\n\n\n")
+
+#String Manipulation
+print("Hello World \nHello World")
