5610c2c8eaf0f22e208f49650932bfe83d56d363
gkishalan <princekishalan655@gmail.com>
2026-07-14 12:42:35 +0530

    Initial commit

---
+++ Day1/task1.py
@@ -0,0 +1,9 @@
+#Printing the Console in Python 
+print("Hello World!")
+
+#Printing Practice 
+print("Mix 500g of floor , 10g yeast and 300ml water in a bowel")
+print("Knead the dough for 10 minutes")
+print("Add 3g of salt")
+print("Leave to rise for 2 hours")
+print("Bake at 200 degrees C for 30 Minutes")
